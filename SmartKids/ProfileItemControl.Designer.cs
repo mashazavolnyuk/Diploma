f55276a0dc1e417cd76c968ba5cd7b0382a3a4b9
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2014-02-26 12:19:46 +0200

    add ProfileItem Control

---
+++ SmartKids/ProfileItemControl.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SmartKids
+{
+    partial class ProfileItemControl
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ProfileItemControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ProfileItemControl";
+            this.Size = new System.Drawing.Size(140, 170);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SmartKids/ProfileItemControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SmartKids
+{
+    public partial class ProfileItemControl : UserControl
+    {
+        public ProfileItemControl()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            Thread.Sleep(4);
+            Colors c = (Colors)r.Next(0,9);
+            BackColor = Color.FromName(c.ToString());
+
+            pictureBox1.Click += pictureBox1_Click;
+        }
+
+        void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (Click != null)
+                Click(this, e);
+        }
+
+        public event EventHandler Click;
+
+
+        public Image Image
+        {
+            get { return pictureBox1.Image; }
+            set { pictureBox1.Image = value; }
+        }
+
+        public string Text
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+    }
+
+    enum Colors
+    {
+        Pink,
+        Green,
+        Blue,
+        Navy,
+        Yellow,
+        Orange,
+        Coral,
+        Indigo,
+        Red
+    }
+}
