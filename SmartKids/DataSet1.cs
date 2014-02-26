fbb2c9dc195a33743965c4cc11f173340f2cbddc
natashalysakova <natashalysakova@gmail.com>
2014-02-26 22:29:45 +0200

    Fuck it!

--- SmartKids/DataSet1.cs
+++ SmartKids/DataSet1.cs
@@ -1,15 +1,29 @@
-﻿using SmartKids.Enums;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Linq;
+using SmartKids.Enums;
+using SmartKids.Properties;
 
 namespace SmartKids
 {
-
-
     public partial class DataSet1
     {
+        public void SaveChanges()
+        {
+            AcceptChanges();
+            WriteXml(Resources.dataBasePath);
+        }
+
+
+        internal List<string> Get_SUB_Cat_name()
+        {
+            return (from DataRow d in Subcategort.Rows select d[Subcategort.nameColumn].ToString()).ToList();
+        }
+
+        partial class CategotyDataTable
+        {
+        }
+
         partial class SubcategortDataTable
         {
         }
@@ -18,9 +32,6 @@ namespace SmartKids
         {
         }
 
-        partial class CategotyDataTable
-        {
-        }
         #region Работа с левелами
 
         //ДОБАВИТЬ категорию
@@ -31,29 +42,26 @@ namespace SmartKids
             row[Categoty.imagePathColumn] = path;
             Categoty.Rows.Add(row);
             SaveChanges();
-
         }
 
 
         //ДОБАВИТЬ подкатегорию
-        public void SubCategory(int category, string name_subcat, string path)
+        public void SubCategory(int category, string nameSubcat, string path)
         {
-
             DataRow row = Subcategort.NewRow();
-            row[Subcategort.nameColumn] = name_subcat;
+            row[Subcategort.nameColumn] = nameSubcat;
             row[Subcategort.id_catColumn] = category;
             row[Subcategort.imagePathColumn] = path;
             Subcategort.Rows.Add(row);
             SaveChanges();
-
         }
 
         //получение id оталкиваясь от name 
         public int Search_ID_CAT(string name_cat)
         {
             int id = (from DataRow IT in Categoty.Rows
-                      where IT[Categoty.nameColumn] == name_cat
-                      select (int)IT[Categoty.cat_idColumn]).ToList()[0];
+                where IT[Categoty.nameColumn] == name_cat
+                select (int) IT[Categoty.cat_idColumn]).ToList()[0];
             return id;
         }
 
@@ -62,8 +70,8 @@ namespace SmartKids
         public string Search_IMAGE_CAT(string name_cat)
         {
             string name_resurs = (from DataRow IT in Categoty.Rows
-                                  where IT[Categoty.nameColumn] == name_cat
-                                  select (string)IT[Categoty.imagePathColumn]).ToList()[0];
+                where IT[Categoty.nameColumn] == name_cat
+                select (string) IT[Categoty.imagePathColumn]).ToList()[0];
             return name_resurs;
         }
 
@@ -81,8 +89,8 @@ namespace SmartKids
         private int Get_ID_TASK(string Sub_name)
         {
             int id = (from DataRow IT in Subcategort.Rows
-                      where IT[Subcategort.nameColumn] == Sub_name
-                      select (int)IT[Subcategort.sub_idColumn]).ToList()[0];
+                where IT[Subcategort.nameColumn] == Sub_name
+                select (int) IT[Subcategort.sub_idColumn]).ToList()[0];
             return id;
         }
 
@@ -92,28 +100,29 @@ namespace SmartKids
         }
 
 
-
         internal List<int> GetCat_ID()
         {
-            return (from DataRow d in Categoty.Rows select (int)d[Categoty.cat_idColumn]).ToList();
+            return (from DataRow d in Categoty.Rows select (int) d[Categoty.cat_idColumn]).ToList();
         }
-
-
 
 
         internal string Get_Resurs(int p)
         {
-            return (from DataRow d in Categoty.Rows where (int)d[Categoty.cat_idColumn] == p select d[Categoty.imagePathColumn].ToString()).ToList()[0];
+            return
+                (from DataRow d in Categoty.Rows
+                    where (int) d[Categoty.cat_idColumn] == p
+                    select d[Categoty.imagePathColumn].ToString()).ToList()[0];
         }
-
-
 
         #endregion
 
         #region Работа с юзерами
+
         public bool New_User(string Name, string Pass, Gender g)
         {
-            List<string> rows = (from DataRow U in Users.Rows where U[Users.user_nameColumn].ToString() == Name select U[Users.user_nameColumn].ToString()).ToList();
+            List<string> rows = (from DataRow U in Users.Rows
+                where U[Users.user_nameColumn].ToString() == Name
+                select U[Users.user_nameColumn].ToString()).ToList();
             if (rows.Contains(Name))
                 return false;
             DataRow newUser = Users.NewRow();
@@ -129,15 +138,16 @@ namespace SmartKids
         private int Search_ID_User(string name_user)
         {
             int id = (from DataRow IT in Users.Rows
-                      where IT[Users.user_nameColumn] == name_user
-                      select (int)IT[Users.user_idColumn]).ToList()[0];
+                where IT[Users.user_nameColumn] == name_user
+                select (int) IT[Users.user_idColumn]).ToList()[0];
             return id;
         }
 
         internal void DeleteUser(int id)
         {
             //пишем запрос в котором получаем все записи с user_id == нашему удаляемому ID 
-            DataRow temp = (from DataRow row in Users.Rows where (int)row[Users.user_idColumn] == id select row).ToList()[0];
+            DataRow temp =
+                (from DataRow row in Users.Rows where (int) row[Users.user_idColumn] == id select row).ToList()[0];
 
             Users.Rows.Remove(temp);
             SaveChanges();
@@ -146,40 +156,24 @@ namespace SmartKids
         #endregion
 
         #region Проверка авторизации
-        internal bool Open_YES(string Name_user,string Pass)
+
+        internal bool Open_YES(string Name_user, string Pass)
         {
-            string s = (from DataRow name in Users.Rows where name[Users.user_nameColumn].ToString() == Name_user select name[Users.passColumn].ToString()).ToList()[0];
-            if (s == Pass)
-                return true;
-            else
+            List<string> list = (from DataRow name in Users.Rows
+                where name[Users.user_nameColumn].ToString() == Name_user
+                select name[Users.passColumn].ToString()).ToList();
+
+            if (list.Count != 0)
+            {
+                string s = list[0];
+                if (s == Pass)
+                    return true;
                 return false;
-                
+            }
 
+            return false;
         }
-
-
-
 
         #endregion
-
-        public void SaveChanges()
-        {
-            AcceptChanges();
-            WriteXml(Properties.Resources.dataBasePath);
-        }
-
-
-
-
-        internal List<string> Get_SUB_Cat_name()
-        {
-            return (from DataRow d in Subcategort.Rows select d[Subcategort.nameColumn].ToString()).ToList();
-        }
-
-
-
-
-
-        
     }
 }
---
+++ SmartKids/Win8Form.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SmartKids
+{
+    public class Win8Form : Form
+    {
+        private readonly Timer _closeTimer = new Timer();
+        private readonly Timer _openTimer = new Timer();
+        private Win8BackButton _win8BackButton1;
+
+        private int step = 40;
+        private int width, height;
+        
+        public Win8Form()
+        {
+            InitializeComponent();
+
+            _closeTimer.Tick += close_Tick;
+            _closeTimer.Interval = 10;
+
+            _openTimer.Tick += open_Tick;
+            _openTimer.Interval = 10;
+
+            width = Screen.PrimaryScreen.Bounds.Width;
+            height = Screen.PrimaryScreen.Bounds.Height;
+
+
+        }
+
+        private void open_Tick(object sender, EventArgs e)
+        {
+            int pos;
+
+            if (Location.X < step)
+                pos = 0;
+            else
+                pos = Location.X - step;
+
+
+            Location = new Point(pos, Location.Y);
+
+
+            if (Location.X == 0)
+            {
+                _openTimer.Enabled = false;
+            }
+        }
+
+        private void close_Tick(object sender, EventArgs e)
+        {
+            Location = new Point(Location.X + step, Location.Y);
+
+
+            if (Location.X > width)
+            {
+                _closeTimer.Enabled = false;
+                Close();
+            }
+        }
+
+
+        private void InitializeComponent()
+        {
+            this._win8BackButton1 = new SmartKids.Win8BackButton();
+            this.SuspendLayout();
+            // 
+            // _win8BackButton1
+            // 
+            this._win8BackButton1.AutoSize = true;
+            this._win8BackButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this._win8BackButton1.BackColor = System.Drawing.Color.Transparent;
+            this._win8BackButton1.Location = new System.Drawing.Point(50, 65);
+            this._win8BackButton1.Name = "_win8BackButton1";
+            this._win8BackButton1.Size = new System.Drawing.Size(47, 47);
+            this._win8BackButton1.TabIndex = 0;
+            this._win8BackButton1.Click += new System.EventHandler(this._win8BackButton1_Click);
+            // 
+            // Win8Form
+            // 
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this._win8BackButton1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Win8Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Win8Form_FormClosing);
+            this.Load += new System.EventHandler(this.MyForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        void Win8Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Location.X == 0)
+            {
+                this.Size = new Size(width, height);
+                _closeTimer.Enabled = true;
+            }
+
+
+            if (!(Location.X >= width))
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+            Location = new Point(width,0);
+            this.Size = new Size(width, height);
+            _openTimer.Enabled = true;
+        }
+
+        private void _win8BackButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
